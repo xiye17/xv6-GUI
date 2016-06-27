bc950e99c11515fc5bfedd2186f7275a75eb8e3d
yexithu <502000201@qq.com>
2016-06-27 15:45:43 +0800

    Remove static_assert reference

--- mkfs.c
+++ mkfs.c
@@ -66,8 +66,6 @@ main(int argc, char *argv[])
   struct dinode din;
 
 
-  static_assert(sizeof(int) == 4, "Integers must be 4 bytes!");
-
   if(argc < 2){
     fprintf(stderr, "Usage: mkfs fs.img files...\n");
     exit(1);
